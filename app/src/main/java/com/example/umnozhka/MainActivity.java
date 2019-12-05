295882349436b202a6c07a7ec3822f7c7da5e1fa
kamar19 <kaimar@mail.ru>
2019-12-05 12:04:25 +0600

    Initial commit

---
+++ app/src/main/java/com/example/umnozhka/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.umnozhka;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+
+public class MainActivity extends AppCompatActivity {
+//    String[] data = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"};
+//
+//    GridView gvMain;
+//    ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        adapter = new ArrayAdapter<String>(this, R.layout.item, R.id.item_TextView, data);
+//        gvMain = (GridView) findViewById(R.id.gridView1);
+//        gvMain.setAdapter(adapter);
+//        adjustGridView();
+    }
+//    private void adjustGridView() {
+//    }
+
+
+
+//                android:padding="20dp"
+//                android:layout_margin="50dp"
+//               <Space
+//                    android:layout_width="wrap_content"
+//                    android:layout_height="wrap_content" />
+
+}
